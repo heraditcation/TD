9f6effb4d837e0ce5c5d1c61138727c780114ed9
Andrew Picornell <picornellandrew@gmail.com>
2020-10-01 22:30:25 +0200

    TD3 v2

---
+++ td3/ex04.py
@@ -0,0 +1,26 @@
+"""
+__author__ = "Andrew PICORNELL"
+__date__ = "Jeudi 1 Octobre"
+__copyright__ = "Copyright 2020, All rights reserved"
+__version__ = "1.0.0"
+__email__ = "andrew.picornell@etu.u-pec.fr // picornellandrew@gmail.com"
+__github__ = "https://github.com/heraditcation"
+__linkedin__ = "https://www.linkedin.com/in/andrewpicornell/"
+__status__ = "C/Python Dev + Master Git/Shell (zsh/sh)"
+"""
+
+liste=[17, 38, 10, 25, 72]
+liste.sort()
+print(liste)
+liste.extend([12])
+print(liste)
+liste.reverse()
+print(liste)
+liste.index(17)
+print(liste)
+liste.remove(38)
+print(liste)
+print(liste[1:3])
+print(liste[:2])
+print(liste[-3:])
+print(liste[0:])
---
+++ td3/ex05.py
@@ -0,0 +1,14 @@
+"""
+__author__ = "Andrew PICORNELL"
+__date__ = "Jeudi 1 Octobre"
+__copyright__ = "Copyright 2020, All rights reserved"
+__version__ = "1.0.0"
+__email__ = "andrew.picornell@etu.u-pec.fr // picornellandrew@gmail.com"
+__github__ = "https://github.com/heraditcation"
+__linkedin__ = "https://www.linkedin.com/in/andrewpicornell/"
+__status__ = "C/Python Dev + Master Git/Shell (zsh/sh)"
+"""
+
+L = [1,2,2,1,2,1]
+L = list(set(L))
+print(L)
